003db2d9e5315b4404103e7996db6ed06c0b8377
Мартин Јанев <150262130+MartinJ1st@users.noreply.github.com>
2025-03-03 15:22:55 +0100

    git commit

--- Predavanje2/Predavanje2.py
+++ Predavanje2/Predavanje2.py
@@ -1,6 +1,6 @@
 import cv2
 
-image = cv2.imread("screencapture-mensa-org-mensa-iq-challenge-2024-11-09-18_41_28.png")
+image = cv2.imread("mensa.png")
 grayscale = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 cv2.imshow("Default", image)
 cv2.imshow("grayscale.png", grayscale)
